7dc19d337e20f534e199bfa0184c4bcf0da043f5
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-12-12 11:59:31 +0100

    Update 2016-12-12T10:59:27.891Z

--- src/index.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-exports.Listener = exports.listener = require('./listener')
-exports.Dialer = exports.dialer = require('./dialer')
-exports.matchSemver = require('./listener/match-semver')
--- src/listener/match-exact.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-function matchExact (myProtocol, senderProtocol, callback) {
-  const result = myProtocol === senderProtocol
-  callback(null, result)
-}
-
-module.exports = matchExact
